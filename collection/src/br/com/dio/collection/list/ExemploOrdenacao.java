5b59cd1831ecc1f52003d81373a3aefc8c0ac697
lorenzo-danesi <ldallacortedanesi@gmail.com>
2022-11-15 15:40:38 -0300

    add ExemploOrdenacao.java

---
+++ collection/src/br/com/dio/collection/list/ExemploOrdenacao.java
@@ -0,0 +1,62 @@
+package br.com.dio.collection.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*Dadas as seguintes informações sobre gatos, crie uma lista e ordene esta lista exibindo:
+(nome - idade(em meses) - cor);
+
+Gato 1 = nome: Jon, idade: 18, cor: preto
+Gato 2 = nome: Simba, idade: 6, cor: tigrado
+Gato 3 = nome: Jon, idade: 12, cor: amarelo
+*/
+
+public class ExemploOrdenacao {
+    public static void main(String[] args) {
+        List<Gato> gatos = new ArrayList<>(){{
+            add(new Gato("Jon", 18, "preto"));
+            add(new Gato("Simba", 6, "amarelo"));
+            add(new Gato("Jon", 12, "tigrado"));
+        }};
+        System.out.println("--\tOrdem de Inserção\t---");
+        System.out.println(gatos);
+
+        System.out.println("--\tOrdem aleatória\t---");
+        Collections.shuffle(gatos);
+        //responsável por 'bagunçar' os valores da lista
+        System.out.println(gatos);
+    }
+}
+class Gato {
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
+}
